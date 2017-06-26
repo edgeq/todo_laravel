c799844290b903060ff19df27ba1b14546fe3bbb
bobbravo2 <bob@mycircletree.com>
2017-06-26 11:44:41 -0400

    7.1 demos

--- class-content/07-week/01-FirstServer/server.js
+++ class-content/07-week/01-FirstServer/server.js
@@ -4,6 +4,10 @@ var http = require("http");
 // Here we define a port to listen to
 var PORT = 8080;
 
+// Here we use the Node HTTP package to create our server.
+// We then pass it the handleRequest function to empower it with functionality.
+var server = http.createServer(handleRequest);
+
 // Here we create a generic function to handle requests and responses
 function handleRequest(request, response) {
 
@@ -11,9 +15,6 @@ function handleRequest(request, response) {
   response.end("It Works!! Path Hit: " + request.url);
 }
 
-// Here we use the Node HTTP package to create our server.
-// We then pass it the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
 
 // Here we start our server so that it can begin listening to client requests.
 server.listen(PORT, function() {
