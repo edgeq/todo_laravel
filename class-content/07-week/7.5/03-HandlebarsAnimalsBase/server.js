3126c050a23a42e34bc75271f5640166d22e6e84
bobbravo2 <bob@mycircletree.com>
2017-06-30 14:00:22 -0400

    7.5.4 solutions

--- class-content/07-week/7.5/03-HandlebarsAnimalsBase/server.js
+++ class-content/07-week/7.5/03-HandlebarsAnimalsBase/server.js
@@ -10,6 +10,7 @@ app.set("view engine", "handlebars");
 var animals = [
   {
     animalType: "dog",
+    animalName: "Dorian Gray",
     pet: true,
     fierceness: 4
   }, {
@@ -35,15 +36,16 @@ app.get("/dog", function(req, res) {
   // Handlebars requires an object to be sent to the dog.handlebars file. Lucky for us, animals[0] is an object!
 
   // 1. Send the dog object from the animals array to the dog.handlebars file.
-  res.render('dog');
+  console.log('a dog', animals[0]);
+  res.render('dog', animals[0]);
 
 });
 
-app.get("/all-pets", function(req, res) {
+app.get("/", function(req, res) {
   // Handlebars requires an object to be sent to the index.handlebars file.
 
   // 2. Send the animals to the index.handlebars file. Remember that animals is an array and not an object.
-
+  res.render('index', {animalList: animals});
 });
 
 app.get("/all-non-pets", function(req, res) {
